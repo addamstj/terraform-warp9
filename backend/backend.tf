62962a96f2b8280ae8930a6559899e96b1f6e484
tjaddams <toby.addams@gmail.com>
2020-07-06 17:10:19 +0100

    Updated code

--- backend/backend.tf
+++ backend/backend.tf
@@ -3,7 +3,7 @@ terraform {
         key = "terraform/tfstate.tfstate"
         bucket = "tj-remote-backend-2020"
         region = "eu-west-2"
-        access_key = "AKIA53SMBKOTA7N4C3PE"
-        secret_key = "0O2HR+z1e+9jaD7CwMkABa+7eLJ/D1i3qaE/Xj3w"
+        access_key = "your_access_key"
+        secret_key = "your_secret_key"
     }
 }
